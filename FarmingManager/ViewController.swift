784cd36260d4cb4700425dfd81e2fbdece08a513
Montoya <alexandrej92@gmail.com>
2017-05-29 22:42:24 +0200

    Initial Commit

---
+++ FarmingManager/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FarmingManager
+//
+//  Created by Alexandre Montoya on 29/05/2017.
+//  Copyright © 2017 Alexandre Montoya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
